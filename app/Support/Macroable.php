511db6c702a2ae65c82aaf1c93b2c31d38788a8d
Elijah <elijahsharper1@gmail.com>
2018-02-23 13:54:40 +0300

    Simple macros

--- app/Support/Macroable.php
+++ app/Support/Macroable.php
@@ -2,7 +2,23 @@
 
 namespace App\Support;
 
+use Closure;
+
 trait Macroable
 {
+  protected static $macros = [];
 
+  public static function macro($name, $macro)
+  {
+    static::$macros[$name] = $macro;
+  }
+
+  public function __call($method, $parameters)
+  {
+    $macro = static::$macros[$method];
+    
+    if ($macro instanceof Closure) {
+      return call_user_func_array($macro, $parameters);
+    }
+  }
 }
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Http\Request;
+
 require_once 'vendor/autoload.php';
+
+Request::macro('getMethod', function() {
+  return 'works';
+});
+
+$request = new Request();
+
+dump($request->getMethod());
